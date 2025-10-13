91e403ec742fd99606155a2eca00760b1e8306b1
xym <2464668802@qq.com>
2025-10-13 19:04:25 +0800

    feat:部门管理

--- src/views/System/components/ManagerDialog.vue
+++ src/views/System/components/ManagerDialog.vue
@@ -41,6 +41,16 @@
           </el-radio-group>
         </el-form-item>
       </el-form>
+      <el-form-item v-if="dialogProps.title !== '重置'" label="所属部门" prop="departId">
+        <el-cascader
+          v-model="dialogProps.row!.departId"
+          :props="{ value: 'id', label: 'name', emitPath: false, checkStrictly: true }"
+          placeholder="请选择管理员所属部门"
+          :options="departmentList"
+          :show-all-levels="false"
+          filterable
+        />
+      </el-form-item>
     </div>
     <template #footer>
       <slot name="footer">
@@ -58,6 +68,9 @@ import { Dialog } from '@/components/Dialog'
 import { getRoleList } from '@/api/modules/role'
 import { getManagerInfoApi } from '@/api/modules/manager'
 import { useAppStoreWithOut } from '@/store/modules/app'
+import { useDepartmentStore } from '@/store/modules/department'
+const departmentStore = useDepartmentStore()
+const departmentList = departmentStore.departmentList
 const appStore = useAppStoreWithOut()
 interface DialogProps {
   title: string
